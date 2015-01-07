a00c9706e6241274d5dc0c982c8360b604b27f50
Jeff Sill <jeff.sill@gmail.com>
2015-01-07 10:06:30 -0800

    Adding mobile menu functionality

--- js/site-functions.js
+++ js/site-functions.js
@@ -1,5 +1,12 @@
 (function ($) {
 
+  Drupal.behaviors.hamburger = {
+    attach: function(context) {
+      $('.hamburger').click(function() {
+        $('#block-system-main-menu').toggleClass('show');
+      });
+    }
+  };
 
 })(jQuery);
 
